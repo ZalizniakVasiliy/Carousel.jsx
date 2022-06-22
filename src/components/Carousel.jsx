8819937b40b8a53c13525dfafdfcd43ecaa59b65
Vasilii Zalizniak <vzalizniak31@gmail.com>
2022-06-22 18:13:00 +0300

    Changed switching class active in elements

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import cn from "classnames";
 
 class Carousel extends React.Component {
     constructor(props) {
@@ -6,53 +7,58 @@ class Carousel extends React.Component {
         this.state = {
             images: this.props.images,
             indexImg: 0
-        }
+        };
     }
 
-    switchNextImg = () => {
-        this.setState(({indexImg, images}) => (
-                {indexImg: indexImg === images.length - 1 ? 0 : ++indexImg}
-            )
-        );
+    switchImg = (arrowSwitch) => () => {
+        let {indexImg, images} = this.state;
+        if (arrowSwitch === `next`) {
+            this.setState({indexImg: indexImg === images.length - 1 ? 0 : ++indexImg})
+        } else {
+            this.setState({indexImg: indexImg === 0 ? images.length - 1 : --indexImg})
+        }
     };
 
-    switchPrevImg = () => {
-        this.setState(({indexImg, images}) => (
-                {indexImg: indexImg === 0 ? images.length - 1 : --indexImg}
-            )
-        );
-    };
+    switchNextImg = this.switchImg('next');
+    switchPrevImg = this.switchImg('prev');
 
     render() {
-        let {images, indexImg} = this.state;
+        const {indexImg, images} = this.state
 
         return (
             <div id="carousel" className="carousel slide" data-bs-ride="carousel">
                 <div className="carousel-inner">
-                    <div className='carousel-item active'>
-                        <img alt="" className="d-block w-100" src={images[indexImg]}/>
-                    </div>
-                    <div className='carousel-item'>
-                        <img alt="" className="d-block w-100" src={images[++indexImg]}/>
-                    </div>
-                    <div className='carousel-item'>
-                        <img alt="" className="d-block w-100" src={images[++indexImg]}/>
-                    </div>
+
+                    {images.map((item, index) => {
+                        const activeImg = cn('carousel-item',
+                            {active: indexImg === index}
+                        );
+
+                        return (
+                            <div className={activeImg} key={index}>
+                                <img className="d-block w-100 " src={item} alt=""></img>
+                            </div>
+                        );
+                    })}
                 </div>
-                <button className="carousel-control-prev"
-                        data-bs-target="#carousel"
-                        type="button"
-                        data-bs-slide="prev"
-                        onClick={this.switchPrevImg}
+
+                <button
+                    className="carousel-control-prev"
+                    data-bs-target="#carousel"
+                    type="button"
+                    data-bs-slide="prev"
+                    onClick={this.switchPrevImg}
                 >
                     <span className="carousel-control-prev-icon" aria-hidden="true"></span>
                     <span className="visually-hidden">Previous</span>
                 </button>
-                <button className="carousel-control-next"
-                        data-bs-target="#carousel"
-                        type="button"
-                        data-bs-slide="next"
-                        onClick={this.switchNextImg}
+
+                <button
+                    className="carousel-control-next"
+                    data-bs-target="#carousel"
+                    type="button"
+                    data-bs-slide="next"
+                    onClick={this.switchNextImg}
                 >
                     <span className="carousel-control-next-icon" aria-hidden="true"></span>
                     <span className="visually-hidden">Next</span>
